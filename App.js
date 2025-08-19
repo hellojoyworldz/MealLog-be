6c3308a18fc1728b3a1f4577f0d279656959dc5c
Seung Hwang <2018066@naver.com>
2025-08-20 00:26:11 +0900

    환경세팅

---
+++ App.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import indexRouter from "./routes/index.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/api", indexRouter);
