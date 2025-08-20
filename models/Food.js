3a811509e03bacf85cae3e2fd59ab1ebfbf8b3fa
Seung Hwang <2018066@naver.com>
2025-08-21 01:13:36 +0900

    푸드 스키마 생성

---
+++ models/Food.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const FoodSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    calories: { type: Number, required: true },
+    nutrients: {
+      carbs: { type: Number, required: true },
+      protein: { type: Number, required: true },
+      fat: { type: Number, required: true },
+      sugar: { type: Number, required: true },
+    },
+    amount: { type: Number }, // grams
+  },
+  { timestamps: true }
+);
+
+foodSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.__v;
+  delete obj.updatedAt;
+  delete obj.createdAt;
+  return obj;
+};
+
+export default mongoose.model("Food", FoodSchema);
